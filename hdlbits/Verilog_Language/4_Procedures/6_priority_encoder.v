2227779602b92b267572e82a4999637bc70f2493
gavin wu <recursionwu@163.com>
2022-03-08 21:43:58 +0800

    Maintain | format code

--- hdlbits/Verilog_Language/4_Procedures/6_priority_encoder.v
+++ hdlbits/Verilog_Language/4_Procedures/6_priority_encoder.v
@@ -24,3 +24,19 @@ module top_module (
         endcase
     end
 endmodule
+
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input [3:0] in,
+    output reg [1:0] pos  );
+
+    always @(*) begin
+        casez (in)
+            4'bzzz1: pos = 2'b0;
+            4'bzz1z: pos = 2'b1;
+            4'bz1zz: pos = 2'b10;
+            4'b1zzz: pos = 2'b11;
+            default: pos = 2'b0;
+        endcase
+    end
+endmodule
