8aa343a2fde4d82a1e5d3421689d5576a8ea727b
di.wu <di.wu@csdental.com>
2022-03-09 12:19:28 +0800

    Maitnain | format code

---
+++ hdlbits/Verilog_Language/5_More_Verilog_Features/4_Combinational_For_Loop.v
@@ -0,0 +1,12 @@
+module top_module(
+    input [99:0] in,
+    output [99:0] out
+);
+    genvar i;
+    generate
+        for (i = 0; i < 100; i = i + 1) begin: somename
+            assign out[i] = in[100 - i -1];
+        end
+    endgenerate
+
+endmodule
